be04d2734133a32c9a8570539a525ddf0a2f3dae
ModischFabrications <modisch.fabrications@gmail.com>
2023-10-18 18:55:22 +0200

    remove unused button import, fix pinout with proper pins (PBs are just offsets)

--- src/pinout.h
+++ src/pinout.h
@@ -3,22 +3,30 @@
 #include <Arduino.h>
 
 /**
- * Uses pins that are available both on Arduino Nano and barebones Attiny85 to simplify testing
+ * Using pins that are available both on Arduino Nano and barebones Attiny45/85 
+ * to simplify testing isn't possible (RX/TX, ...), so we remap here
  * https://components101.com/sites/default/files/component_pin/Arduino-Nano-Pinout.png
  * https://circuits-diy.com/wp-content/uploads/2021/11/attiny85-pinout.jpg
  * */
 
 namespace Pinout {
 
+#ifdef DEBUG
 // LOW is pressed, using buildin pullups
-const uint8_t BUTTONS = PB3;
+const uint8_t BUTTONS = A0;
 
 // low while charging, might be bypassed via LED
-const uint8_t IO_CHARGING = PB4;
+const uint8_t IO_CHARGING = 12;
 
 // clock is optional, data can also be used to PWM
-const uint8_t DATA_LEDS = PB1;
-const uint8_t CLOCK_LEDS = PB2;
+const uint8_t DATA_LEDS = 9;
+const uint8_t CLOCK_LEDS = 10;
+#else
+const uint8_t BUTTONS = 3;
+const uint8_t IO_CHARGING = 4;
+const uint8_t DATA_LEDS = 1;
+const uint8_t CLOCK_LEDS = 2;
+#endif
 
 void setup() {
     // analog does not need a pinMode
@@ -30,4 +38,4 @@ void setup() {
     pinMode(CLOCK_LEDS, OUTPUT);
 }
 
-} // namespace Pins
+} // namespace Pinout
